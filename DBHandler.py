8ee4ce04c8a71ad783ee77272d042e1228db7ca7
ayutaz <ka1357amnbpdr@gmail.com>
2022-05-12 04:23:22 +0900

    feat: タスク管理機能の変更

--- DBHandler.py
+++ DBHandler.py
@@ -7,19 +7,14 @@ class DBHandler:
     def __init__(self, notion_token: str):
         self.notion_token = notion_token
         self.notion = Client(auth=self.notion_token)
+        self.today = datetime.datetime.today()
 
-    def get_doing_task_db(self, db_id: str) -> dict:
+    def get_doing_task_db(self, db_id: str):
         db = self.notion.databases.query(
             **{
                 'database_id': db_id,
                 "filter": {
                     "and": [
-                        {
-                            "property": "期日",
-                            "date": {
-                                "is_not_empty": True
-                            }
-                        },
                         {
                             "property": "期限",
                             "select": {
@@ -32,7 +27,7 @@ class DBHandler:
         )
         return db['results']
 
-    def get_done_task_db(self, db_id: str) -> dict:
+    def get_done_task_db(self, db_id: str):
         db = self.notion.databases.query(
             **{
                 'database_id': db_id,
@@ -71,6 +66,57 @@ class DBHandler:
             }
         )
 
+    def update_task_status(self, page_id: str, priority: str, deadline: str) -> None:
+        self.notion.pages.update(
+            page_id,
+            properties={
+                'Priority': {
+                    'select': {
+                        'name': priority
+                    }
+                },
+                '期限': {
+                    'select': {
+                        'name': deadline
+                    }
+                }
+            }
+        )
+
+    def get_task_priority(self, deadline_date) -> str:
+        # 3日以内の場合は、高
+        # 3日以上7日以内の場合は、中
+        # 7日以上の場合は、低
+        if deadline_date is None:
+            return '低'
+        if deadline_date <= self.today + datetime.timedelta(days=3):
+            return '高'
+        elif self.today + datetime.timedelta(days=3) < deadline_date <= self.today + datetime.timedelta(days=7):
+            return '中'
+        else:
+            return '低'
+
+    def get_task_deadline(self, deadline_date) -> str:
+        # 1日以内の場合は、dau
+        # 2日以上7日以内の場合は、week
+        # 7日以上の場合は、none
+
+        if deadline_date is None:
+            return 'none'
+        if deadline_date <= self.today + datetime.timedelta(days=1):
+            return 'day'
+        elif deadline_date <= self.today + datetime.timedelta(days=7):
+            return 'week'
+        else:
+            return 'none'
+
     @staticmethod
     def get_page_id(db_result) -> str:
         return db_result['id']
+
+    @staticmethod
+    def get_task_deadline_date(db_result):
+        if db_result['properties']['期日']['date'] is None:
+            return None
+        else:
+            return datetime.datetime.strptime(db_result['properties']['期日']['date']['start'], '%Y-%m-%d')
--- Main.py
+++ Main.py
@@ -14,7 +14,22 @@ def TaskDoneWatch(db_id) -> None:
         db_handler.update_task_date(page_id)
 
 
+def TaskPriorityWatch(db_id) -> None:
+    results = db_handler.get_doing_task_db(db_id)
+    for idx in range(len(results)):
+        page_id = db_handler.get_page_id(results[idx])
+        deadline_data = db_handler.get_task_deadline_date(results[idx])
+        new_task_priority = db_handler.get_task_priority(deadline_data)
+        new_task_deadline = db_handler.get_task_deadline(deadline_data)
+        db_handler.update_task_status(page_id, new_task_priority, new_task_deadline)
+
+
+def TaskWatch(db_id) -> None:
+    TaskDoneWatch(db_id)
+    TaskPriorityWatch(db_id)
+
+
 db_handler = DBHandler(os.getenv("NOTION_TOKEN"))
 
-TaskDoneWatch(os.getenv("PRIVATE_DB"))
-TaskDoneWatch(os.getenv("WORK_DB"))
+TaskWatch(os.getenv("PRIVATE_DB"))
+TaskWatch(os.getenv("WORK_DB"))
